bbaf06f170670371dec8f5323a1a65eafa4f57c6
sneha-tamasha <sneha@tamasha.live>
2024-06-03 02:34:36 +0530

    removed unused code

--- app/src/main/java/com/example/drivedemo/presentation/util/GoogleDriveHolder.kt
+++ app/src/main/java/com/example/drivedemo/presentation/util/GoogleDriveHolder.kt
@@ -1,13 +1,7 @@
 package com.example.drivedemo.presentation.util
 
-import com.google.api.client.util.DateTime
-
 
 class GoogleDriveFileHolder {
     var id: String? = null
     var name: String? = null
-    var modifiedTime: DateTime? = null
-    var size: Long = 0
-    var createdTime: DateTime? = null
-    var starred: Boolean? = null
 }
